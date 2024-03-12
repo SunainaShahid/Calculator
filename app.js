5fab08c0d5db19c70fa1efc9020d2d18cb3346d8
Sunaina Shahid <S@gmail.com>
2024-03-13 01:36:01 +0500

    javascript file added

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
+
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+
 
 function calculate() {
     try {
